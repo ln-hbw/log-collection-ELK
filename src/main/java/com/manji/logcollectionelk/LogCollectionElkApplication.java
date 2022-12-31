e3baf7cee8d28815c75124998e1a6c4ac247a422
manji <hbw1224@163.com>
2022-12-31 11:42:10 +0800

    init

---
+++ src/main/java/com/manji/logcollectionelk/LogCollectionElkApplication.java
@@ -0,0 +1,13 @@
+package com.manji.logcollectionelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogCollectionElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogCollectionElkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manji/logcollectionelk/LogCollectionElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manji.logcollectionelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogCollectionElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
